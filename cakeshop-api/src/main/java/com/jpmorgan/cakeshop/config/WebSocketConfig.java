52d3f7b15cc99a628df3feb4dbb9e8c029336377
Alberto Hernandez <albherna@gmail.com>
2018-02-27 13:37:45 +0100

    Fix SockJS Library Bug

--- cakeshop-api/src/main/java/com/jpmorgan/cakeshop/config/WebSocketConfig.java
+++ cakeshop-api/src/main/java/com/jpmorgan/cakeshop/config/WebSocketConfig.java
@@ -6,41 +6,43 @@ import org.springframework.web.socket.config.annotation.AbstractWebSocketMessage
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
-
 /**
  *
  * @author Michael Kazansky
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
+{
 
-    @Override
-     public void configureMessageBroker(MessageBrokerRegistry  registry) {
-        registry.enableSimpleBroker("/topic");
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry)
+	{
+		registry.enableSimpleBroker("/topic");
 
-//        registry.enableStompBrokerRelay("/topic")
-//                .setAutoStartup(true)
-//                .setSystemHeartbeatReceiveInterval(5000)
-//                .setSystemHeartbeatSendInterval(5000)
-//
-//        registry.enableStompBrokerRelay("/topic/currentLocation")
-//                .setRelayPort(properties.getStompPort())
-//                .setClientLogin(properties.getUsername())
-//                .setClientPasscode(properties.getPassword())
-//                .setSystemLogin(properties.getUsername())
-//                .setSystemPasscode(properties.getPassword());
+		// registry.enableStompBrokerRelay("/topic")
+		// .setAutoStartup(true)
+		// .setSystemHeartbeatReceiveInterval(5000)
+		// .setSystemHeartbeatSendInterval(5000)
+		//
+		// registry.enableStompBrokerRelay("/topic/currentLocation")
+		// .setRelayPort(properties.getStompPort())
+		// .setClientLogin(properties.getUsername())
+		// .setClientPasscode(properties.getPassword())
+		// .setSystemLogin(properties.getUsername())
+		// .setSystemPasscode(properties.getPassword());
 
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+		registry.setApplicationDestinationPrefixes("/app");
+	}
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry
-            .addEndpoint("ws")
-            .setAllowedOrigins("*")
-            .withSockJS()
-            .setClientLibraryUrl("/cakeshop/js/vendor/sockjs-1.0.3.min.js");
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry)
+	{
+		registry
+				.addEndpoint("ws")
+				.setAllowedOrigins("*")
+				.withSockJS()
+				.setClientLibraryUrl("/cakeshop/js/vendor/sockjs-1.1.4.min.js");
+	}
 
 }
